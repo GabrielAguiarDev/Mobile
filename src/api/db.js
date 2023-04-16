74b49263f5ab56859dfa9dd5845720f6a14bb770
Gabriel <gabriel.aguiar.dev@gmail.com>
2023-04-16 02:17:59 -0300

    removendo .env

--- src/api/db.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios"
-
-export default axios.create({
-  baseURL: `${process.env.BASE_URL}`,
-})
-
-export const api_bible = axios.create({
-  baseURL: process.env.URL_BIBLE,
-})
